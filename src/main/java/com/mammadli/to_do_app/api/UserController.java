4894aa334299b9f50df8c9f48eff47a63ecdab29
mammadli <gulnar.mammadli@ut.ee>
2022-11-15 12:54:36 +0200

    UserController updated

--- src/main/java/com/mammadli/to_do_app/api/UserController.java
+++ src/main/java/com/mammadli/to_do_app/api/UserController.java
@@ -2,11 +2,10 @@ package com.mammadli.to_do_app.api;
 
 import com.mammadli.to_do_app.db.entity.User;
 import com.mammadli.to_do_app.services.UserServices;
+import com.mammadli.to_do_app.util.ResponseData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,15 +15,18 @@ public class UserController {
     private final UserServices userServices;
 
     @PostMapping
-    ResponseEntity<User> createUser(@RequestBody User user){
+    ResponseEntity<ResponseData<User>> createUser(@RequestBody User user){
         return ResponseEntity.ok(userServices.createUser(user));
     }
 
     @GetMapping("/{idUser}")
-    ResponseEntity<User> getUser(@PathVariable String idUser){
+    ResponseEntity<ResponseData<User>> getUser(@PathVariable String idUser){
         return ResponseEntity.ok(userServices.getUser(idUser));
     }
 
-
+    @DeleteMapping("/{idUser}")
+    ResponseEntity<ResponseData<String>> deleteUser(@PathVariable String idUser){
+       return ResponseEntity.ok(userServices.deleteUser(idUser));
+    }
 
 }
