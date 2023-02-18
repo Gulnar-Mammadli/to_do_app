f39cf6027ea0a96d67e34b4fe51eec86ce7cb355
mammadli <gulnar.mammadli@ut.ee>
2023-02-18 21:42:56 +0200

    Initial commit

---
+++ src/main/java/com/mammadli/to_do_app/model/dto/TaskRequest.java
@@ -0,0 +1,15 @@
+package com.mammadli.to_do_app.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskRequest {
+
+
+}
