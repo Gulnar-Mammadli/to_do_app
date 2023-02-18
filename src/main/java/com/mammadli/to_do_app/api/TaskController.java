b26e4bbbe5d105e5a1ed26761b36aa00dc9aaa90
mammadli <gulnar.mammadli@ut.ee>
2023-02-18 21:43:22 +0200

    Update all endpoints

--- src/main/java/com/mammadli/to_do_app/api/TaskController.java
+++ src/main/java/com/mammadli/to_do_app/api/TaskController.java
@@ -1,6 +1,7 @@
 package com.mammadli.to_do_app.api;
 
-import com.mammadli.to_do_app.db.entity.Task;
+import com.mammadli.to_do_app.model.dto.TaskRequest;
+import com.mammadli.to_do_app.model.entity.Task;
 import com.mammadli.to_do_app.services.TaskServices;
 import com.mammadli.to_do_app.util.ResponseData;
 import lombok.RequiredArgsConstructor;
@@ -13,30 +14,31 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/tasks")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4200", "http://localhost:8080"})
 public class TaskController {
 
     private final TaskServices taskServices;
 
 
     @PostMapping("/add")
-    ResponseEntity<ResponseData<Task>> addTask( @RequestBody Task task){
-        return ResponseEntity.ok(taskServices.addTask(task));
+    Task addTask(@RequestBody TaskRequest request) {
+        return taskServices.addTask(request);
     }
 
-//    TODO update this method
-    @PutMapping("/update/")
-    ResponseEntity<ResponseData<Task>> updateTask(@RequestBody Task task){
-        return ResponseEntity.ok(taskServices.updateTask(task));
+    //    TODO update this method
+    @PutMapping("/update/{taskId}")
+    Task updateTask(@RequestBody TaskRequest request, String taskId) {
+        return taskServices.updateTask(request, taskId));
     }
 
     @GetMapping("/get-list/{idUser}")
-    ResponseEntity<ResponseData<List<Task>>> getAllTasks(@PathVariable String idUser){
-        return ResponseEntity.ok(taskServices.getAllTasks(idUser));
+    List<Task> getAllTasks(@PathVariable String idUser) {
+        return taskServices.getAllTasks(idUser);
     }
 
     @DeleteMapping("/{idUser}/{id}")
-    ResponseEntity<ResponseData<String>> deleteTask(@PathVariable String idUser,@PathVariable String id){
-        return ResponseEntity.ok(taskServices.deleteTask(idUser, id));
+    Void deleteTask(@PathVariable String idUser, @PathVariable String id) {
+        return taskServices.deleteTask(idUser, id);
     }
 
 
