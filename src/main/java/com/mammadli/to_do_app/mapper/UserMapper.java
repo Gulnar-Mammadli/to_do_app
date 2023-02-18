2cc31783acacbd9ccda73deb4436371acb944817
mammadli <gulnar.mammadli@ut.ee>
2023-02-18 21:17:59 +0200

    Add fromUpdateRequest mapper

--- src/main/java/com/mammadli/to_do_app/mapper/UserMapper.java
+++ src/main/java/com/mammadli/to_do_app/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.mammadli.to_do_app.mapper;
 
 import com.mammadli.to_do_app.model.dto.RegistrationRequest;
+import com.mammadli.to_do_app.model.dto.UpdateRequest;
 import com.mammadli.to_do_app.model.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -11,5 +12,8 @@ public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     User mapToUser(RegistrationRequest registrationRequest);
+
+    User fromUpdateRequest(UpdateRequest request);
+
 
 }
