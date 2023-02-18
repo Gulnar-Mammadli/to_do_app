c394205c169f42dca4970f8060b87ddc5c872009
mammadli <gulnar.mammadli@ut.ee>
2023-02-18 22:05:23 +0200

    Initial commit

--- src/main/java/com/mammadli/to_do_app/model/dto/UpdateRequest.java
+++ src/main/java/com/mammadli/to_do_app/model/dto/UpdateRequest.java
@@ -1,9 +1,15 @@
 package com.mammadli.to_do_app.model.dto;
 
 import com.mammadli.to_do_app.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateRequest {
 
     private String username;
